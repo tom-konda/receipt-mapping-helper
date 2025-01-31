d6dc45ed04d86edb842c7c82834568a6ab97fdfd
Tom Konda <tom.konda.dev@gmail.com>
2025-01-31 19:12:14 +0900

    Remove no need output

--- src/App.svelte
+++ src/App.svelte
@@ -16,14 +16,6 @@
       <ListWrapper />
     </div>
   </section>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
