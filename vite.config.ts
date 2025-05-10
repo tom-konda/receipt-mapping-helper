d9bae1c894b856b44e86736e8f6ff269fb17222f
Tom Konda <tom.konda.dev@gmail.com>
2025-05-10 20:17:10 +0900

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
+const isGithub = process.env.IS_GITHUB;
+const base = isGithub?.length ? '/receipt-mapping-helper/' : undefined;
+
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/receipt-mapping-helper/',
+  base,
   plugins: [svelte()],
 })
